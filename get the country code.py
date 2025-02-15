f5f4a6159bdb3a3c5e2f923074a03cf19c5fd327
aitihyazz <Your GitHub Email>
2025-02-15 23:00:49 +0600

    3

---
+++ get the country code.py
@@ -0,0 +1,5 @@
+contrycode={"India":"0091","Australia":"0025","Nepla":"00977"}
+print("Country code of india is")
+print(contrycode.get("India","not found"))
+print("Country code of Japan is")
+print(contrycode.get("japan","not found"))
